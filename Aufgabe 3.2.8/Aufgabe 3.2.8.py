424ae2433d3c83e2e78e14af4334bac9c258dc0e
Ruffy93 <rafael-neumann@web.de>
2025-05-27 14:26:16 +0200

    Änderrung 3.2.8 (Zahlen(n)

--- Aufgabe 3.2.8/Aufgabe 3.2.8.py
+++ Aufgabe 3.2.8/Aufgabe 3.2.8.py
@@ -1,12 +1,12 @@
-zahlenn = [4, 7, 2, 9, 1, 5, 3, 23, 14, 324, 423, 4645, 6785]
+zahlen = [4, 7, 2, 9, 1, 5, 3, 23, 14, 324, 423, 4645, 6785]
 
-print("Länge:", len(zahlenn))
-print("Größe:", max(zahlenn))
-print("Kleinste Zahl:", min(zahlenn))
+print("Länge:", len(zahlen))
+print("Größe:", max(zahlen))
+print("Kleinste Zahl:", min(zahlen))
 
-mittelwert = sum(zahlenn) / len(zahlenn)
+mittelwert = sum(zahlen) / len(zahlen)
 print("Mittelwert:", mittelwert)
-print("umgekehrt:", list(reversed(zahlenn)))
+print("umgekehrt:", list(reversed(zahlen)))
 
-gerade_zahlen = [zahl for zahl in zahlenn if zahl % 2 == 0]
+gerade_zahlen = [zahl for zahl in zahlen if zahl % 2 == 0]
 print("Gerade Zahlen:", gerade_zahlen)
